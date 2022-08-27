584af4b8481ae434002bccf99b9f3aef87666996
ZHKO1 <zhko11993@gmail.com>
2022-08-27 23:16:04 +0800

    refactor Pixel FIFO

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,10 +1,12 @@
 use crate::memory::Memory;
-use crate::ppu::PpuStatus::{DRAWING, HBLANK, OAMSCAN, VBLANK};
+use crate::ppu::FetcherStatus::{GetTile, GetTileDataHigh, GetTileDataLow};
+use crate::ppu::PixelType::{Sprite, Window, BG};
+use crate::ppu::PpuStatus::{Drawing, HBlank, OAMScan, VBlank};
 use crate::util::check_bit;
 use std::collections::VecDeque;
 use std::{cell::RefCell, rc::Rc};
 
-const WIDTH: usize = 256;
+const WIDTH: usize = 160;
 const HEIGHT: usize = 144;
 
 enum Color {
@@ -15,54 +17,187 @@ enum Color {
 }
 
 pub enum PpuStatus {
-    OAMSCAN = 2,
-    DRAWING = 3,
-    HBLANK = 0,
-    VBLANK = 1,
+    OAMScan = 2,
+    Drawing = 3,
+    HBlank = 0,
+    VBlank = 1,
 }
 
-struct Pixel {}
+#[derive(Clone, Copy)]
+enum PixelType {
+    BG,
+    Window,
+    Sprite,
+}
+
+#[derive(Clone, Copy)]
+struct Pixel {
+    ptype: PixelType,
+    pvalue: u8,
+}
+
+enum FetcherStatus {
+    GetTile,
+    GetTileDataLow,
+    GetTileDataHigh,
+}
 
 struct Fetcher {
-    index: u16,
+    scan_x: u16,
+    scan_y: u16,
+    cycles: u16,
     mmu: Rc<RefCell<dyn Memory>>,
-    // result: Vec<u8>,
+    status: FetcherStatus, // result: Vec<u8>,
+    tile_index: u16,
+    tile_data_low: u8,
+    tile_dada_high: u8,
+    buffer: Vec<Pixel>,
 }
 impl Fetcher {
     fn new(mmu: Rc<RefCell<dyn Memory>>) -> Self {
-        Fetcher { index: 0, mmu }
+        Fetcher {
+            scan_x: 0,
+            scan_y: 0,
+            mmu,
+            cycles: 0,
+            status: GetTile,
+            tile_index: 0,
+            tile_data_low: 0,
+            tile_dada_high: 0,
+            buffer: Vec::new(),
+        }
     }
-    fn get(&self, ly: u16) -> Vec<u8> {
-        let lcdc = self.mmu.borrow().get(0xff40);
-        // let window_title_map_area = check_bit(lcdc, 6);
+    fn init(&mut self, x: u16, y: u16) {
+        self.scan_x = x;
+        self.scan_y = y;
+        self.cycles = 0;
+        self.status = GetTile;
+
+        self.tile_index = 0;
+        self.tile_data_low = 0;
+        self.tile_dada_high = 0;
+
+        self.buffer = Vec::new();
+    }
+    fn trick(&mut self) {
+        if self.cycles == 1 {
+            self.cycles = 0;
+            return;
+        }
+        self.cycles += 1;
+        match self.status {
+            GetTile => {
+                self.tile_index = self.get_tile();
+                self.status = GetTileDataLow;
+            }
+            GetTileDataLow => {
+                self.tile_data_low = self.get_tile_data_low();
+                self.status = GetTileDataHigh;
+            }
+            GetTileDataHigh => {
+                self.tile_dada_high = self.get_tile_data_high();
+                self.buffer = self.get_buffer();
+                self.status = GetTile;
+            }
+        }
+    }
+    fn get_tile(&self) -> u16 {
+        let lcdc = self.mmu.borrow().get(0xFF40);
+        let scy = self.mmu.borrow().get(0xFF42);
+        let scx = self.mmu.borrow().get(0xFF43);
+        let scx = 0;
+        let scy = 0;
         let bg_window_tile_area = check_bit(lcdc, 4);
         let bg_tile_map_area = check_bit(lcdc, 3);
-        let (bg_map_start, bg_map_end): (u16, u16) = match bg_tile_map_area {
+        let (bg_map_start, _): (u16, u16) = match bg_tile_map_area {
             true => (0x9C00, 0x9FFF),
             false => (0x9800, 0x9BFF),
         };
-        let mut result: Vec<u8> = vec![];
-        for draw_x in 0..WIDTH {
-            let bg_map_x = draw_x as u16 / 8;
-            let bg_map_y = ly / 8;
-            let bg_map_index = bg_map_x + bg_map_y * 32;
-            let bg_map_byte = self.mmu.borrow().get(bg_map_start + bg_map_index);
-            let tile_index: u16 = if bg_window_tile_area {
-                0x8000 + bg_map_byte as u16 * 8 * 2
-            } else {
-                (0x9000 as i32 + (bg_map_byte as i8) as i32 * 8 * 2) as u16
-            };
-            let tile_pixel_x = draw_x % 8;
-            let tile_pixel_y = ly % 8;
-            let tile_byte_low = self.mmu.borrow().get(tile_index + tile_pixel_y * 2);
-            let tile_byte_high = self.mmu.borrow().get(tile_index + tile_pixel_y * 2 + 1);
-            let pixel_bit = 8 - tile_pixel_x - 1;
-            let pixel_low = tile_byte_low & (1 << pixel_bit) == (1 << pixel_bit);
-            let pixel_high = tile_byte_high & (1 << pixel_bit) == (1 << pixel_bit);
-            let pixel = (pixel_low as u8) + (pixel_high as u8) << 1;
-            result.push(pixel);
+        let bg_map_x = (self.scan_x + scx as u16) / 8;
+        let bg_map_y = (self.scan_y + scy as u16) / 8;
+        let bg_map_index = bg_map_x + bg_map_y * 32;
+        let bg_map_byte = self.mmu.borrow().get(bg_map_start + bg_map_index);
+        let tile_index: u16 = if bg_window_tile_area {
+            0x8000 + bg_map_byte as u16 * 8 * 2
+        } else {
+            (0x9000 as i32 + (bg_map_byte as i8) as i32 * 8 * 2) as u16
+        };
+        tile_index
+    }
+    fn get_tile_data_low(&self) -> u8 {
+        let tile_index = self.tile_index;
+        let tile_pixel_y = self.scan_y % 8;
+        let tile_byte_low = self.mmu.borrow().get(tile_index + tile_pixel_y * 2);
+        tile_byte_low
+    }
+    fn get_tile_data_high(&self) -> u8 {
+        let tile_index = self.tile_index;
+        let tile_pixel_y = self.scan_y % 8;
+        let tile_byte_high = self.mmu.borrow().get(tile_index + tile_pixel_y * 2 + 1);
+        tile_byte_high
+    }
+    fn get_buffer(&mut self) -> Vec<Pixel> {
+        let mut result = Vec::new();
+        for buffer_index in 0..8 {
+            let pixel_bit = 8 - buffer_index - 1;
+            let pixel_low = self.tile_data_low & (1 << pixel_bit) == (1 << pixel_bit);
+            let pixel_high = self.tile_dada_high & (1 << pixel_bit) == (1 << pixel_bit);
+            let pvalue = (pixel_low as u8) + (pixel_high as u8) << 1;
+            result.push(Pixel { ptype: BG, pvalue });
         }
-        return result;
+        result
+    }
+}
+
+struct FIFO {
+    scan_x: u16,
+    scan_y: u16,
+    fetcher: Fetcher,
+    queue: VecDeque<Pixel>,
+}
+
+impl FIFO {
+    fn new(mmu: Rc<RefCell<dyn Memory>>) -> Self {
+        let fetcher = Fetcher::new(mmu.clone());
+        FIFO {
+            scan_x: 0,
+            scan_y: 0,
+            fetcher,
+            queue: VecDeque::new(),
+        }
+    }
+    fn init(&mut self, y: u16) {
+        self.scan_x = 0;
+        self.scan_y = y;
+        self.fetcher.init(0, y);
+    }
+    fn trick(&mut self) -> Option<Pixel> {
+        let result = if self.queue.len() > 8 {
+            self.pop_front()
+        } else {
+            None
+        };
+        if self.fetcher.buffer.len() > 0 {
+            if self.queue.len() <= 8 {
+                self.scan_x += self.fetcher.buffer.len() as u16;
+                for pixel in self.fetcher.buffer.clone().into_iter() {
+                    self.push_back(pixel);
+                }
+                self.fetcher.init(self.scan_x, self.scan_y);
+            }
+        } else {
+            self.fetcher.trick();
+        }
+        result
+    }
+    fn push_back(&mut self, pixel: Pixel) {
+        self.queue.push_back(pixel);
+    }
+    fn pop_front(&mut self) -> Option<Pixel> {
+        self.queue.pop_front()
+    }
+    fn clear(&mut self) {
+        self.queue.clear();
     }
 }
 
@@ -70,54 +205,60 @@ pub struct PPU {
     ly: u16,
     cycles: u32,
     status: PpuStatus,
-    fetcher: Fetcher,
-    fifo: VecDeque<u8>,
+    fifo: FIFO,
     mmu: Rc<RefCell<dyn Memory>>,
-    pub pixel_array: [u32; WIDTH * HEIGHT],
+    ly_buffer: Vec<u32>,
+    pub frame_buffer: [u32; WIDTH * HEIGHT],
 }
 impl PPU {
     pub fn new(mmu: Rc<RefCell<dyn Memory>>) -> Self {
-        let fetcher = Fetcher::new(mmu.clone());
+        let fifo = FIFO::new(mmu.clone());
         PPU {
             ly: 0,
             cycles: 0,
-            status: OAMSCAN,
-            mmu: mmu,
-            fetcher,
-            fifo: VecDeque::new(),
-            pixel_array: [0; WIDTH * HEIGHT],
+            status: OAMScan,
+            mmu,
+            fifo,
+            ly_buffer: Vec::new(),
+            frame_buffer: [0; WIDTH * HEIGHT],
         }
     }
     pub fn trick(&mut self) {
         match self.status {
-            OAMSCAN => {
-                // println!("OAMSCAN");
+            OAMScan => {
+                // println!("OAMScan");
                 if self.cycles == 79 {
-                    self.status = DRAWING;
+                    self.status = Drawing;
+                    self.ly_buffer = Vec::new();
+                    self.fifo.init(self.ly);
                 }
                 self.cycles += 1;
             }
-            DRAWING => {
-                // println!("DRAWING");
-                if self.cycles == 80 {
-                    let array = self.fetcher.get(self.ly);
-                    for (width, pixel) in array.iter().enumerate() {
-                        self.pixel_array[(self.ly as usize * WIDTH + width) as usize] =
-                            self.get_pixel_color(*pixel);
+            Drawing => {
+                // println!("Drawing");
+                let pixel_option = self.fifo.trick();
+                if let Some(pixel) = pixel_option {
+                    self.ly_buffer.push(self.get_pixel_color(pixel.pvalue));
+                    if self.ly_buffer.len() == WIDTH {
+                        for (scan_x, pixel) in self.ly_buffer.iter().enumerate() {
+                            self.frame_buffer[(self.ly as usize * WIDTH + scan_x) as usize] =
+                                *pixel;
+                        }
+                        self.ly_buffer.clear();
+                        self.fifo.clear();
+                        self.status = HBlank;
                     }
-                }
-                if self.cycles == 251 {
-                    self.status = HBLANK;
+                } else {
                 }
                 self.cycles += 1;
             }
-            HBLANK => {
-                // println!("HBLANK");
+            HBlank => {
+                // println!("HBlank");
                 if self.cycles == 455 {
                     if self.ly == 143 {
-                        self.status = VBLANK;
+                        self.status = VBlank;
                     } else {
-                        self.status = OAMSCAN;
+                        self.status = OAMScan;
                     }
                     self.ly += 1;
                     self.cycles = 0;
@@ -125,11 +266,11 @@ impl PPU {
                     self.cycles += 1;
                 }
             }
-            VBLANK => {
-                // println!("VBLANK");
+            VBlank => {
+                // println!("VBlank");
                 if self.cycles == 455 {
                     if self.ly == 153 {
-                        self.status = OAMSCAN;
+                        self.status = OAMScan;
                         self.ly = 0;
                     } else {
                         self.ly += 1;
